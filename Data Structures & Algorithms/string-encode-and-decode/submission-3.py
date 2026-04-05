6d243ab2e021124ad6c50615223761403d5bc194
Zhenwei Ding <dingzwa@gmail.com>
2026-04-05 09:55:45 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + ";" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        start = 0
+
+        while start < len(s):
+            end = start
+            while s[end] != ';':
+                end += 1
+    
+            length = int(s[start:end])
+            start = end + 1
+            end = start+length
+
+            res.append(s[start:end])
+            start = end
+
+        return res
+        
+
+
+            
