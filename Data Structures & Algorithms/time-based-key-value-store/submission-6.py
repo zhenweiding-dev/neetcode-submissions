de4867202c9e70574d9ae170a360798a286f04cb
Zhenwei Ding <dingzwa@gmail.com>
2026-06-03 18:27:16 -0700

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.py
@@ -0,0 +1,30 @@
+class TimeMap():
+    """
+    the result should be like {"alice":{1:"happy",2:"sad"}}
+
+    so initialization creates a set
+    set(key, value, time) add a new key, or add a new value to list
+    get(key, time), returns "" if key not exists 
+    elif time>lenth-1 return index of length - 1 else time as index
+    """
+
+    def __init__(self):
+        self.data = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.data[key] #[[1:'happy'],[]]
+        if not values:
+            return ""
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if values[mid][0] == timestamp:
+                 return values[mid][1]
+            elif values[mid][0] > timestamp:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return values[r][1] if r >= 0 else ""
